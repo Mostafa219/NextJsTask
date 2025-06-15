8d64ffc91617cce777e279d3dbc402a8973d98d8
Mostafa219 <mostafaarafat737@gmail.com>
2025-06-16 02:12:10 +0300

    adding signwithgoogleauth

--- src/app/_lib/dbconnections.jsx
+++ src/app/_lib/dbconnections.jsx
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export function dbConnections() {
   mongoose
     .connect(
-      `mongodb+srv://Mostafaarafat:dMxAOTl7zsFz8hmL@cluster0.7wnwhhk.mongodb.net/todo`
+      `mongodb+srv://Mostafaarafat:${process.env.MONGO_SECRET}@cluster0.7wnwhhk.mongodb.net/todo`
     )
     .then(() => console.log("connected to database"))
     .catch((err) => console.log(`problem yasta the error is ${err}`));
